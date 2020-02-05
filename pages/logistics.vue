92b35751820b1e4201b12ae0af9c42da5787cafb
Dan Moore <dan@enveil.com>
2020-02-04 23:15:35 -0500

    Put venue map on a separate row on mobile

--- pages/logistics.vue
+++ pages/logistics.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row align="center">
-      <v-col md="6" class="text-center">
+      <v-col cols="12" sm="6" class="text-center">
         <div class="travel-header text-center mb-2">Venue</div>
         <p>
           Raspberry Plain Manor<br/>
@@ -16,7 +16,7 @@
         </p>
       </v-col>
 
-      <v-col md="6">
+      <v-col cols="12" sm="6">
         <div class="map-frame">
           <div class="map-container">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3093.6766346767645!2d-77.55052548470691!3d39.1593358795312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b622baee01a4bb%3A0xa0870bccbae41ab6!2sRaspberry%20Plain%20Manor!5e0!3m2!1sen!2sus!4v1579230960293!5m2!1sen!2sus" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
