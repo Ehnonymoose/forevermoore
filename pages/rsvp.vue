56e3252b09e3d0c2788a765871bd0ec3bb202518
Dan Moore <dan@enveil.com>
2020-04-14 21:33:29 -0400

    add a note saying when to RSVP

--- pages/rsvp.vue
+++ pages/rsvp.vue
@@ -1,5 +1,8 @@
 <template>
   <v-row align="center" no-gutters>
+    <v-col cols="12" sm="8" offset-sm="2">
+      <div class="reply-info">Kindly reply by July 5th. Thank you!</div>
+    </v-col>
     <v-col cols="12" sm="8" offset-sm="2" align-content="center">
       <iframe style="display:block;width:100%;height:800px;border:none;overflow:hidden;" 
           src="https://forevermoore.anrsvp.com/?embed=true">
@@ -7,3 +10,12 @@
     </v-col>
   </v-row>
 </template>
+
+<style scoped>
+.reply-info {
+  text-align: center;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  font-size: 1.1rem;
+}
+</style>
